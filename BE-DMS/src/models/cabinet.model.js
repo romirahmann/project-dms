abc7663e1e0d65197dfb123cac6e70b1a94fd5dd
romirahmann <romirahman03romi@gmail.com>
2025-02-17 08:10:19 +0700

    LAST ROUND

--- BE-DMS/src/models/cabinet.model.js
+++ BE-DMS/src/models/cabinet.model.js
@@ -5,29 +5,29 @@ const getAll = async () =>
     .select(
       "c.cabinetId",
       "c.cabinetName",
-      "c.cabinetDescription",
+
       "c.isActive",
       "t.tenantName"
     )
-    .from("tbl_cabinet as c")
+    .from("tbl_cabinets as c")
     .join("tbl_tenants as t", "t.tenantId", "c.tenantId");
 const getById = async (id) =>
   await db
     .select(
       "c.cabinetId",
       "c.cabinetName",
-      "c.cabinetDescription",
+
       "c.isActive",
       "t.tenantName"
     )
-    .from("tbl_cabinet as c")
+    .from("tbl_cabinets as c")
     .join("tbl_tenants as t", "t.tenantId", "c.tenantId")
     .where("c.cabinetId", id);
-const insert = async (data) => await db("tbl_cabinet").insert(data);
+const insert = async (data) => await db("tbl_cabinets").insert(data);
 const update = async (id, data) =>
-  await db("tbl_cabinet").where("cabinetId", id).update(data);
+  await db("tbl_cabinets").where("cabinetId", id).update(data);
 const del = async (id) =>
-  await db("tbl_cabinet").where("cabinetId", id).delete();
+  await db("tbl_cabinets").where("cabinetId", id).delete();
 module.exports = {
   getAll,
   getById,
